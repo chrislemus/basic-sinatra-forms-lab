a01567fab514ce40586dc1dc3a34a3e31a5fa7bb
chrislemus <icrislemus@gmail.com>
2021-01-20 14:54:20 -0500

    commit tree 33d628c5c8d68cc2139e3316f1daee6e8406e341

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get('/newteam') {
+    erb :newteam
+  }
 
+  post('/team') {
+    @name = params['name']
+    @coach = params['coach']
+    @pointGuard = params['pg']
+    @shootingGuard = params['sg']
+    @smallForward = params['sf']
+    @powerForward = params['pf']
+    @center = params['c']
+    erb :team
+  }
 end
